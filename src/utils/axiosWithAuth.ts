8d29a4e4e649d71e967c3c590d40c55f4739b2ce
William Sedlacek <wsedlacekc@gmail.com>
2020-05-21 02:48:52 -0500

    feat: Observable HTTP Request
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/utils/axiosWithAuth.ts
+++ src/utils/axiosWithAuth.ts
@@ -1,12 +1,8 @@
-import axios from 'axios';
+import axios from 'axios-observable';
 import { sessionQuery } from '../state/session';
 
 export const axiosWithAuth = () => {
   const { token } = sessionQuery.getValue();
 
-  return axios.create({
-    headers: {
-      Authorization: token,
-    },
-  });
+  return axios.create({ headers: { Authorization: token } });
 };
