b1e2c64b251f2e5723de15db0fb09abada80130d
William Sedlacek <wsedlacekc@gmail.com>
2020-05-21 02:29:35 -0500

    feat: Routing
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.scss';
 
 import { useSessionFacade } from './hooks';
+
 import { NavBar } from './components/common/NavBar/NavBar';
+import { PublicRoute } from './components/common/PublicRoute';
+import { PrivateRoute } from './components/common/PrivateRoute';
+
+import { Events } from './components/events/Events';
+import { Login } from './components/onboarding/Login';
 
 const App = () => {
   const [{ token }] = useSessionFacade();
@@ -11,7 +17,11 @@ const App = () => {
   return (
     <>
       <Router>
-        <NavBar title="Potluck Planner" user={!!token} />
+        <NavBar title="Potluck Planner" loggedIn={!!token} />
+        <Switch>
+          <PrivateRoute exact path="/" to="/login" component={Events} />
+          <PublicRoute exact path="/login" to="/" component={Login} />
+        </Switch>
       </Router>
     </>
   );
