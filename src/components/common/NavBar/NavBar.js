95f86a5d3576aa615f7ce9951cdac83aafbcec9e
William Sedlacek <wsedlacekc@gmail.com>
2020-05-21 02:29:22 -0500

    feat: Logout
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/components/common/NavBar/NavBar.js
+++ src/components/common/NavBar/NavBar.js
@@ -3,16 +3,18 @@ import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
 import './NavBar.scss';
 
+import { sessionService } from '../../../state/session';
+
 export const NavBar = ({ title, loggedIn }) => {
   return (
     <header>
-      <Link>
+      <Link to="/">
         <span>{title}</span>
       </Link>
       <div className="spacer"></div>
       {loggedIn && (
         <aside className="actions">
-          <Button className="logout" color="link">
+          <Button className="logout" color="link" onClick={() => sessionService.logout()}>
             Logout
           </Button>
         </aside>
