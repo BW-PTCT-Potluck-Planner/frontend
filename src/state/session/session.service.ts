ec089a496d130c92366f9ae1b35e53aea57f7af8
William Sedlacek <wsedlacekc@gmail.com>
2020-05-20 23:39:13 -0500

    feat: Sync token to local storage
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/state/session/session.service.ts
+++ src/state/session/session.service.ts
@@ -1,13 +1,17 @@
 import { SessionStore, sessionStore } from './session.store';
+import { SessionQuery, sessionQuery } from './session.query';
 
 export class SessionService {
-  constructor(private store: SessionStore) {
+  constructor(private store: SessionStore, private query: SessionQuery) {
     this.init();
   }
 
   public init(): void {
     this.store.update({});
+    this.query.select().subscribe(({ token }) => {
+      localStorage.setItem('token', token);
+    });
   }
 }
 
-export const sessionService = new SessionService(sessionStore);
+export const sessionService = new SessionService(sessionStore, sessionQuery);
