88dbcca63642380e0142d4c35ce19b31805f199b
William Sedlacek <wsedlacekc@gmail.com>
2020-05-21 01:52:24 -0500

    feat: Public Route
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

---
+++ src/components/common/PublicRoute.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+import { useSessionFacade } from '../../hooks';
+
+export const PublicRoute = ({ component: Component, to, ...rest }) => {
+  const [{ token }] = useSessionFacade();
+  return <Route {...rest} render={() => (token ? <Redirect to={to} /> : <Component />)} />;
+};
