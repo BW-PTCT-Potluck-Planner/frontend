9dcafb86e7a0ff8adb9d32e404d7938a7babaed3
William Sedlacek <wsedlacekc@gmail.com>
2020-05-20 23:37:00 -0500

    fix: Shorter Import
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/components/events/Events.js
+++ src/components/events/Events.js
@@ -1,6 +1,6 @@
 import React from './node_modules/react';
 
-import { useEventsFacade } from '../../hooks/events.hook';
+import { useEventsFacade } from '../../hooks';
 import { Event } from './Event';
 
 export const Events = () => {
