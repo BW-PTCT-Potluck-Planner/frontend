d8ec2b2b266d6254fcc6e8215f33340197a3239b
Mustafa Hassan <60632959+MustafaCajib@users.noreply.github.com>
2020-05-21 23:08:34 -0700

    Sign in Form showing

--- src/components/onboarding/Login.js
+++ src/components/onboarding/Login.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button, Form, Input, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+
 
 import { sessionService } from '../../state/session';
 
 export const Login = () => {
   return (
-    <>
+    <main>
+    
+    <Form>
+    <h6>Sign in</h6>
+
+      <Label html for = "email" >Email</Label>
+      <Input
+       name = "email"
+       placeholder = "Email"
+       type = "text"
+       
+      />
+      <Label html for = "password">Password</Label>
+      <Input
+       name ="password"
+       placeholder = "Password"
+       type = "text"
+       
+      />
+    
+    
       <Button onClick={() => sessionService.login()}>Login</Button>
+      <div >
       <Link to="/register">No account? Sign Up!</Link>
-    </>
+      </div>
+      </Form>
+    </main>
   );
 };
