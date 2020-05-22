4d93149e8850cc4ce979c300da8a9fb988aabac9
Mustafa Hassan <60632959+MustafaCajib@users.noreply.github.com>
2020-05-22 13:56:44 -0700

    working on validation

--- src/components/onboarding/Login.js
+++ src/components/onboarding/Login.js
@@ -1,31 +1,49 @@
 import React from 'react';
 import { Button, Form, Input, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { sessionService } from './../../state/session';
+
+const schema = yup.object().shape({
+  email: yup.string().email().required("Email is required"),
+  password: yup.string().required("Password is required")
+});
 
 
-import { sessionService } from '../../state/session';
 
 export const Login = () => {
+  
+  const { register, handleSubmit, errors } = useForm({
+    validationSchema: schema
+  });
+  const onSubmit = data => console.log(data);
+
+  
   return (
     <main>
     
-    <Form>
-    <h6>Sign in</h6>
-
+    <Form onSubmit = {handleSubmit(onSubmit)}>
+      
       <Label html for = "email" >Email</Label>
       <Input
        name = "email"
        placeholder = "Email"
-       type = "text"
+       type = "email"
+       ref = {register}
        
       />
+      {errors.email && <p>{errors.email.message}</p>}
+      
+
       <Label html for = "password">Password</Label>
       <Input
        name ="password"
        placeholder = "Password"
-       type = "text"
-       
+       type = "password"
+       ref = {register}
       />
+      {errors.email && <p>{errors.email.message}</p>}
     
     
       <Button onClick={() => sessionService.login()}>Login</Button>
