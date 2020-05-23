ad756f14e894a7440aba6d6d5bb86440a84b578c
Mustafa Hassan <60632959+MustafaCajib@users.noreply.github.com>
2020-05-23 14:47:41 -0700

    fixed validation

--- src/components/onboarding/Login.js
+++ src/components/onboarding/Login.js
@@ -1,50 +1,39 @@
-import React from 'react';
-import { Button, Form, Input, Label } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Button, Form, Input, Label } from "reactstrap";
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import * as yup from "yup";
-import { sessionService } from './../../state/session';
-
-
-
+import { sessionService } from "./../../state/session";
 
 export const Login = () => {
-  
   const { register, handleSubmit, errors } = useForm();
-  
-  const onSubmit = data => console.log(data);
 
-  
+  const onSubmit = (data) => sessionService.login(data);
+
   return (
     <main>
-    
-    <Form onSubmit = {handleSubmit(onSubmit)}>
-      
-      <Label>Email</Label>
-      <Input
-       name = "email"
-       placeholder = "Email"
-       type = "text"
-       ref = {register({required: true})}
-       
-      />
-      {errors.email && <p>Email is required</p>}
-      
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Label>Email</Label>
+        <Input
+          name="email"
+          placeholder="Email"
+          type="text"
+          innerRef={register({ required: true })}
+        />
+        {errors.email && <p>Email is required</p>}
 
-      <Label>Password</Label>
-      <Input
-       name ="password"
-       placeholder = "Password"
-       type = "password"
-       ref = {register({required: true, minLength: 8})}
-      />
-      {errors.password && <p>Password is required</p>}
-    
-    
-      <button onClick={() => sessionService.login()}>Login</button>
-      <div >
-      <Link to="/register">No account? Sign Up!</Link>
-      </div>
+        <Label>Password</Label>
+        <Input
+          name="password"
+          placeholder="Password"
+          type="password"
+          innerRef={register({ required: true })}
+        />
+        {errors.password && <p>Password is required</p>}
+
+        <Button>Login</Button>
+        <div>
+          <Link to="/register">No account? Sign Up!</Link>
+        </div>
       </Form>
     </main>
   );
