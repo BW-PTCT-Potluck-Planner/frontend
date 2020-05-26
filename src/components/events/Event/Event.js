9a6c57fc6d33a20b72619f96961b6c0d027f30b9
Mustafa Hassan <60632959+MustafaCajib@users.noreply.github.com>
2020-05-26 01:39:43 -0700

    event edits updated

--- src/components/events/Event/Event.js
+++ src/components/events/Event/Event.js
@@ -3,10 +3,18 @@ import './Event.scss';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const Event = ({ event: { id, title } }) => {
+export const Event = ({ event: { id, title, date, time, location} }) => {
   return (
+  
     <Link to={`/event/${id}`}>
-      <p>{title}</p>
+      <div>
+      <h3>{title}</h3>
+      <span>{date}</span>
+      <span> @ {time}</span>
+      <span> - {location}</span>
+      <button>Deatils</button>
+      </div>
     </Link>
+   
   );
 };
--- src/components/events/EventEdit/EventEdit.js
+++ src/components/events/EventEdit/EventEdit.js
@@ -44,6 +44,46 @@ export const EventEdit = () => {
             invalid={!!errors.title}
           />
           <FormFeedback>{errors.title?.message}</FormFeedback>
+          <Label for= "date">
+            Date
+          </Label>
+          <Input
+           id = "date"
+           name= "date"
+           type = "date"
+           placeholder = "Date"
+           innerRef = {register({
+             required: 'The date is required'
+           })}
+           invalid={!!errors.date}
+          />
+          <FormFeedback>{errors.date?.message}</FormFeedback>
+          <Label for= "time">
+            Time
+          </Label>
+          <Input
+           id = "time"
+           name= "time"
+           type = "time"
+           placeholder = "Time"
+           innerRef = {register({
+             required: 'The time is required'
+           })}
+           invalid={!!errors.time}
+          />
+         <Label for= "location">
+            Location
+          </Label>
+          <Input
+           id = "location"
+           name= "location"
+           type = "location"
+           placeholder = "Locaction"
+           innerRef = {register({
+             required: 'The location is required'
+           })}
+           invalid={!!errors.location}
+          />
         </FormGroup>
         <Button>{active ? 'Edit' : 'Create'}</Button>
       </Form>
